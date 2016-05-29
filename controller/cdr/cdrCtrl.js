d79f45df941117e7576d8aa5f2c0ed5cf60f6a36
dinusha_dck <dinusha@duosoftware.com>
2016-05-29 21:29:58 +0530

    add next prev buttons

--- controller/cdr/cdrCtrl.js
+++ controller/cdr/cdrCtrl.js
@@ -13,11 +13,37 @@
 
         $scope.recLimit = "10";
 
+        $scope.top = -1;
+        $scope.bottom = -1;
+
         $scope.startDate = new Date();
         $scope.endDate = new Date();
 
-        $scope.getProcessedCDR = function()
+        $scope.offset = -1;
+        $scope.prevOffset = -1;
+
+        $scope.loadNextPage = function()
         {
+            if($scope.bottom)
+            {
+                $scope.offset = $scope.bottom;
+                $scope.prevOffset = $scope.top;
+                $scope.getProcessedCDR($scope.offset);
+            }
+
+        };
+
+
+        $scope.getProcessedCDR = function(offset)
+        {
+            if(!offset)
+            {
+                $scope.top = -1;
+                $scope.bottom = -1;
+
+                $scope.offset = -1;
+                $scope.prevOffset = -1;
+            }
             $scope.cdrList = [];
             var startYear = $scope.startDate.getFullYear().toString();
             var startMonth = ($scope.startDate.getMonth()+1).toString();
@@ -30,19 +56,29 @@
             var startTime = startYear + '-' + startMonth + '-' + startDay + ' 00:00:00%2b05:30';
             var endTime = endYear + '-' + endMonth + '-' + endDay + ' 23:59:59%2b05:30';
 
+            var offset = $scope.offset;
+
             var lim = parseInt($scope.recLimit);
 
-            cdrApiHandler.getCDRForTimeRange(startTime, endTime, lim, null).then(function(cdrResp)
+            cdrApiHandler.getCDRForTimeRange(startTime, endTime, lim, offset).then(function(cdrResp)
             {
                 if(!cdrResp.Exception && cdrResp.IsSuccess && cdrResp.Result)
                 {
+                    var topSet = false;
+                    var bottomSet = false;
+
+                    var count = 0;
+                    var cdrLen = Object.keys(cdrResp.Result).length;
+
                     for(cdr in cdrResp.Result)
                     {
+                        count++;
                         var cdrAppendObj = {};
                         var outLegProcessed = false;
                         var curCdr = cdrResp.Result[cdr];
 
                         var len = curCdr.length;
+
 
                         for(i=0; i<curCdr.length; i++)
                         {
@@ -51,6 +87,18 @@
 
                             if(legDirection === 'inbound')
                             {
+                                if(!topSet)
+                                {
+                                    $scope.top = currCdrLeg.id;
+                                    topSet = true;
+                                }
+
+                                if(!bottomSet && count === cdrLen)
+                                {
+                                    $scope.bottom = currCdrLeg.id;
+                                    bottomSet = true;
+                                }
+
                                 cdrAppendObj.SipFromUser = currCdrLeg.SipFromUser;
                                 cdrAppendObj.SipToUser = currCdrLeg.SipToUser;
                                 cdrAppendObj.IsAnswered = currCdrLeg.IsAnswered;
@@ -76,6 +124,12 @@
                             }
                             else
                             {
+                                if(!bottomSet && count === cdrLen)
+                                {
+                                    $scope.bottom = currCdrLeg.id;
+                                    bottomSet = true;
+                                }
+
                                 cdrAppendObj.RecievedBy = currCdrLeg.SipToUser;
                                 cdrAppendObj.AnswerSec = currCdrLeg.AnswerSec;
 
