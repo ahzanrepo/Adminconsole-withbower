20b1f971f867fb8d7af58cec901fbb46b5debbad
waruna <waruna@duosoftware.com>
2016-06-05 23:48:54 +0530

    remove " tag

--- app.js
+++ app.js
@@ -6,9 +6,9 @@
 var mainApp = angular.module('veeryConsoleApp', ['ngAnimate', 'ui.bootstrap',
     'ui.router', 'ui.checkbox', 'chart.js', 'angular-flot', 'angularMoment',
     'resourceServiceModule', 'ngTagsInput', 'authServiceModule', 'jlareau.pnotify',
-    'easypiechart', 'mgcrea.ngStrap', 'angular.filter', "fileServiceModule", "angularFileUpload", "download", "ngMessages", "ngAudio","bw.paging"]);
+    'easypiechart', 'mgcrea.ngStrap', 'angular.filter', 'fileServiceModule', 'angularFileUpload', 'download', 'ngMessages', 'ngAudio','bw.paging']);
 
-// "fileServiceModule","angularFileUpload","download"
+
 var baseUrls = {
     'resourceServiceBaseUrl': 'http://localhost:8831/DVP/API/6.0/ResourceManager/',
     'ardsmonitoringBaseUrl': 'http://ardsmonitoring.104.131.67.21.xip.io/DVP/API/1.0.0.0/ARDS/',
