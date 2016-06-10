61f805fa4e26fb3782ab81cc9b9bdf35f9798ba8
Damith <dpremakumara@gmail.com>
2016-06-10 17:39:08 +0530

    fix date picker issues

--- app.js
+++ app.js
@@ -161,3 +161,24 @@ mainApp.directive('menuCollapse', function () {
         }
     }
 });
+
+mainApp.directive('datepicker', function () {
+    return {
+        restrict: "A",
+        require: "ngModel",
+        link: function (scope, elem, attrs, ngModelCtrl) {
+            var updateModel = function (dateText) {
+                scope.$apply(function () {
+                    ngModelCtrl.$setViewValue(dateText);
+                });
+            };
+            var options = {
+                dateFormat: "dd-mm-yy",
+                onSelect: function (dateText) {
+                    updateModel(dateText);
+                }
+            };
+            elem.datepicker(options);
+        }
+    }
+});
