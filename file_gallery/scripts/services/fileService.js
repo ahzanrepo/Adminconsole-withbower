4fa9df8d0838ccf59c0b363a0aa2592cb6e0b1db
waruna <waruna@duosoftware.com>
2016-09-02 13:35:45 +0530

    correct url

--- file_gallery/scripts/services/fileService.js
+++ file_gallery/scripts/services/fileService.js
@@ -51,10 +51,24 @@ fileModule.factory("fileService", function ($http, download,authService,baseUrls
     });
   };
 
+  /*
   var searchFiles = function (categoryId,startTime,endTime) {
     return $http({
       method: 'get',
       url: baseUrls.fileServiceUrl+ 'Files/infoByCategoryID/'+categoryId+'?startDateTime='+startTime+'&endDateTime='+endTime,
+      headers: {
+        'authorization': authService.GetToken()
+      }
+    }).then(function (response) {
+      return response.data.Result;
+    });
+  };
+  */
+  var searchFiles = function (categoryId,startTime,endTime) {
+    return $http({
+      method: 'get',
+      url: baseUrls.fileServiceUrl+ 'Files/infoByCategoryID/'+categoryId,
+      params: [{startDateTime: startTime},{endDateTime:endTime}],
       headers: {
         'authorization': authService.GetToken()
       }
