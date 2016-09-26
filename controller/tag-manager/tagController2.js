cb36f332add0a9f0110ad8a34bddefe6139e4e4e
Pawan Sasanka <pawan@duosoftware.com>
2016-09-26 18:29:15 +0530

    Tag tree building with populated objects

--- controller/tag-manager/tagController2.js
+++ controller/tag-manager/tagController2.js
@@ -57,10 +57,10 @@ mainApp.controller('tagcontroller2', function ($scope,$rootScope,$state,$uibModa
     };
 
 
-    $scope.loadTagDetails = function (tagID,callback) {
-        if(tagID)
+    $scope.loadTagDetails = function (tagData,callback) {
+        if(tagData)
         {
-            tagBackendService.getTagDetails(tagID).then(function (response) {
+            tagBackendService.getTagDetails(tagData._id).then(function (response) {
 
                 if(!response.data.IsSuccess)
                 {
