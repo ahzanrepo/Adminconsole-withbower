ef4383a02a5780f81701b69122f492c23e9d7462
waruna <waruna@duosoftware.com>
2016-10-18 11:22:37 +0530

    add deleted list

--- socialConnectors/scripts/controllers/socialFbConnectorController.js
+++ socialConnectors/scripts/controllers/socialFbConnectorController.js
@@ -206,14 +206,15 @@ mainApp.controller('socialFbConnectorController', function FormBuilderCtrl($scop
         socialConnectorService.ActivateFacebookAccount(page._id,data).then(function (response) {
             if(response){
                 $scope.GetFacebookAccounts();
-                $scope.showAlert("Remove FB Page", 'success',"Successfully Remove Page from System.");
+                $scope.showAlert("FB Page", 'success',"Page Added Back To System.");
             }
             else{
-                $scope.showAlert("Remove FB Page", 'error',"Fail To Remove Page.");
+                $scope.showAlert("FB Page", 'error',"Fail To Add.");
             }
         }, function (error) {
             console.error("AddFacebookPageToSystem err");
             $scope.isLoading = false;
+            $scope.showAlert("FB Page", 'error',"Fail To Add.");
         });
 
 
