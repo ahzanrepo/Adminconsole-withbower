dbc3f5fa17426e4011a2861297dc7402f0d14bc2
Damith <dpremakumara@gmail.com>
2016-10-18 11:03:25 +0530

    app.js error fixed

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ var baseUrls = {
     'autoattendantUrl': 'http://autoattendant.app.veery.cloud/DVP/API/1.0.0.0/',
     'TrunkServiceURL': 'http://phonenumbertrunkservice.app.veery.cloud/DVP/API/1.0.0.0/',
     'socialConnectorUrl':'http://localhost:4647/DVP/API/1.0.0.0/Social/',
-    'notification': 'http://notificationservice.app.veery.cloud/'
+    'notification': 'http://notificationservice.app.veery.cloud/',
     'appointment': 'http://limithandler.app.veery.cloud/DVP/API/1.0.0.0/LimitAPI/Schedule/Appointment/'
 
 
