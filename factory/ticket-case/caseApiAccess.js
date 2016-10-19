6eade9d9ae01f34794262d408b57e8d2e3e92e45
heshanindika <k.heshan.indika@gmail.com>
2016-10-19 14:46:40 +0530

    Initiate case and case Configuration uis

---
+++ factory/ticket-case/caseApiAccess.js
@@ -0,0 +1,87 @@
+/**
+ * Created by Heshan.i on 10/19/2016.
+ */
+
+(function(){
+
+    var caseApiAccess = function($http, baseUrls) {
+
+        //----------------------------------Case Configuration----------------------------------------------------------------------
+
+        var createCaseConfiguration = function(caseConfigInfo){
+            return $http({
+                method: 'POST',
+                url: baseUrls.ticketUrl+'CaseConfiguration',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                data: caseConfigInfo
+            }).then(function(response){
+                return response.data;
+            });
+        };
+
+        var deleteCaseConfiguration = function(caseConfigId){
+            return $http({
+                method: 'DELETE',
+                url: baseUrls.ticketUrl+'CaseConfiguration/'+caseConfigId,
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(function(response){
+                return response.data;
+            });
+        };
+
+        //----------------------------------Case----------------------------------------------------------------------
+
+        var createCase = function(caseInfo){
+            return $http({
+                method: 'POST',
+                url: baseUrls.ticketUrl+'Case',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                data: caseInfo
+            }).then(function(response){
+                return response.data;
+            });
+        };
+
+        var addTicketToCase = function(caseId, ticketId){
+            return $http({
+                method: 'PUT',
+                url: baseUrls.ticketUrl+'Case/'+caseId+'/RelatedTicket/'+ticketId,
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(function(response){
+                return response.data;
+            });
+        };
+
+        var deleteCase = function(caseId){
+            return $http({
+                method: 'DELETE',
+                url: baseUrls.ticketUrl+'Case/'+caseId,
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(function(response){
+                return response.data;
+            });
+        };
+
+        return{
+            createCaseConfiguration: createCaseConfiguration,
+            deleteCaseConfiguration: deleteCaseConfiguration,
+            createCase: createCase,
+            addTicketToCase: addTicketToCase,
+            deleteCase: deleteCase
+        };
+    };
+
+    var module = angular.module('veeryConsoleApp');
+    module.factory('caseApiAccess', caseApiAccess);
+
+}());
