404de86f179af4eec717f0bfca24215bc7e6982c
Damith <dpremakumara@gmail.com>
2016-11-08 14:17:40 +0530

    captcha

--- auth/controller/signUpCtrl.js
+++ auth/controller/signUpCtrl.js
@@ -2,7 +2,7 @@
  * Created by Damith on 6/18/2016.
  */
 
-mainApp.controller('signUpCtrl', function ($rootScope, $scope, $state, signUpServices,$auth) {
+mainApp.controller('signUpCtrl', function ($rootScope, $scope, $state, signUpServices, $auth, baseUrls) {
 
     //go to login
     $scope.onClickLogIn = function () {
@@ -42,35 +42,40 @@ mainApp.controller('signUpCtrl', function ($rootScope, $scope, $state, signUpSer
         $scope.isSignUp = true;
 
         $auth.signup(newUser)
-            .then(function(response) {
+            .then(function (response) {
                 //$auth.setToken(response);
                 showAlert('Job Done', 'success', 'Registration successfully please check email for verification...');
                 $state.go('login');
             })
-            .catch(function(response) {
+            .catch(function (response) {
                 showAlert('Error', 'error', 'User Registration error...');
                 $scope.isSignUp = false;
             });
 
         /*
-        signUpServices.createNewUser(newUser, function (result) {
-            if (result) {
-                var organisation = {
-                    username: $scope.userName,
-                    password: $scope.password,
-                    organisationName: $scope.userName
-                };
-                signUpServices.createOrganisation(organisation, function (result) {
-                    if (result) {
-                        showAlert('Job Done', 'success', 'Registration successfully...');
-                        $state.go('login');
-                    }
-                });
-            } else {
-                showAlert('Error', 'error', 'User Registration error...');
-                $scope.isSignUp = false;
-            }
-        });*/
+         signUpServices.createNewUser(newUser, function (result) {
+         if (result) {
+         var organisation = {
+         username: $scope.userName,
+         password: $scope.password,
+         organisationName: $scope.userName
+         };
+         signUpServices.createOrganisation(organisation, function (result) {
+         if (result) {
+         showAlert('Job Done', 'success', 'Registration successfully...');
+         $state.go('login');
+         }
+         });
+         } else {
+         showAlert('Error', 'error', 'User Registration error...');
+         $scope.isSignUp = false;
+         }
+         });*/
+    }
+
+    $scope.recaptcha = {
+        siteKey: baseUrls.siteKey,
+        SecretKey: baseUrls.secretKey
     }
 
 
@@ -110,61 +115,61 @@ mainApp.directive('passwordVerify', function () {
 
 
 mainApp.directive('passwordStrength', [
-        function() {
-            return {
-                require: 'ngModel',
-                restrict: 'E',
-                scope: {
-                    password: '=ngModel'
-                },
+    function () {
+        return {
+            require: 'ngModel',
+            restrict: 'E',
+            scope: {
+                password: '=ngModel'
+            },
 
-                link: function(scope, elem, attrs, ctrl) {
-                    scope.$watch('password', function(newVal) {
+            link: function (scope, elem, attrs, ctrl) {
+                scope.$watch('password', function (newVal) {
 
-                        scope.strength = isSatisfied(newVal && newVal.length >= 8) +
-                            isSatisfied(newVal && /[A-z]/.test(newVal)) +
-                            isSatisfied(newVal && /(?=.*\W)/.test(newVal)) +
-                            isSatisfied(newVal && /\d/.test(newVal));
+                    scope.strength = isSatisfied(newVal && newVal.length >= 8) +
+                        isSatisfied(newVal && /[A-z]/.test(newVal)) +
+                        isSatisfied(newVal && /(?=.*\W)/.test(newVal)) +
+                        isSatisfied(newVal && /\d/.test(newVal));
 
-                        function isSatisfied(criteria) {
-                            return criteria ? 1 : 0;
-                        }
-                    }, true);
-                },
-                template: '<div class="progress">' +
-                '<div class="progress-bar progress-bar-danger" style="width: {{strength >= 1 ? 25 : 0}}%"></div>' +
-                '<div class="progress-bar progress-bar-warning" style="width: {{strength >= 2 ? 25 : 0}}%"></div>' +
-                '<div class="progress-bar progress-bar-warning" style="width: {{strength >= 3 ? 25 : 0}}%"></div>' +
-                '<div class="progress-bar progress-bar-success" style="width: {{strength >= 4 ? 25 : 0}}%"></div>' +
-                '</div>'
-            }
+                    function isSatisfied(criteria) {
+                        return criteria ? 1 : 0;
+                    }
+                }, true);
+            },
+            template: '<div class="progress">' +
+            '<div class="progress-bar progress-bar-danger" style="width: {{strength >= 1 ? 25 : 0}}%"></div>' +
+            '<div class="progress-bar progress-bar-warning" style="width: {{strength >= 2 ? 25 : 0}}%"></div>' +
+            '<div class="progress-bar progress-bar-warning" style="width: {{strength >= 3 ? 25 : 0}}%"></div>' +
+            '<div class="progress-bar progress-bar-success" style="width: {{strength >= 4 ? 25 : 0}}%"></div>' +
+            '</div>'
         }
-    ]);
+    }
+]);
 
 mainApp.directive('patternValidator', [
-        function() {
-            return {
-                require: 'ngModel',
-                restrict: 'A',
-                link: function(scope, elem, attrs, ctrl) {
-                    ctrl.$parsers.unshift(function(viewValue) {
+    function () {
+        return {
+            require: 'ngModel',
+            restrict: 'A',
+            link: function (scope, elem, attrs, ctrl) {
+                ctrl.$parsers.unshift(function (viewValue) {
 
-                        var patt = new RegExp(attrs.patternValidator);
+                    var patt = new RegExp(attrs.patternValidator);
 
-                        var isValid = patt.test(viewValue);
+                    var isValid = patt.test(viewValue);
 
-                        ctrl.$setValidity('passwordPattern', isValid);
+                    ctrl.$setValidity('passwordPattern', isValid);
 
-                        // angular does this with all validators -> return isValid ? viewValue : undefined;
-                        // But it means that the ng-model will have a value of undefined
-                        // So just return viewValue!
-                        return viewValue;
+                    // angular does this with all validators -> return isValid ? viewValue : undefined;
+                    // But it means that the ng-model will have a value of undefined
+                    // So just return viewValue!
+                    return viewValue;
 
-                    });
-                }
-            };
-        }
-    ]);
+                });
+            }
+        };
+    }
+]);
 
 
 
