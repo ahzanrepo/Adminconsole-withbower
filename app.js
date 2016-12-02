f0d1564a2629d3dbd80e867f79bc479d5d3abf48
waruna@duosoftware.com <waruna@duosoftware.com>
2016-12-02 16:40:49 +0530

    chg url

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ var baseUrls = {
     'dashBordUrl': 'http://dashboard.app.veery.cloud/',
     'autoattendantUrl': 'http://autoattendant.app.veery.cloud/DVP/API/1.0.0.0/',
     'TrunkServiceURL': 'http://phonenumbertrunkservice.app.veery.cloud/DVP/API/1.0.0.0/',
-    'socialConnectorUrl': 'http://localhost:4647/DVP/API/1.0.0.0/Social/',
+    'socialConnectorUrl': 'http://104.236.197.119:4647/DVP/API/1.0.0.0/Social/',
     'notification': 'http://notificationservice.app.veery.cloud/',
     'cdrProcessor': 'http://cdrprocessor.app.veery.cloud/DVP/API/1.0.0.0/CallCDR/',
     'limitHandlerUrl': 'http://limithandler.app.veery.cloud/DVP/API/1.0.0.0/',
