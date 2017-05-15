eb0a9c2cbdc9bcec31374d32223c5ef66e2890e2
sukitha <sukitha@duosoftware.com>
2017-05-15 13:21:12 +0530

    changes

--- directives/subscribe/subscribeServices.js
+++ directives/subscribe/subscribeServices.js
@@ -77,49 +77,49 @@ mainApp.factory('subscribeServices', function (baseUrls, loginService) {
     };
 
     var unsubscribe = function (view) {
-        if (view == "dashoboard") {
-            SE.unsubscribe({room: 'QUEUE:QueueDetail'});
-            SE.unsubscribe({room: 'QUEUE:CurrentCount'});
-            SE.unsubscribe({room: 'QUEUE:TotalCount'});
-            SE.unsubscribe({room: 'QUEUE:TotalCount'});
-            SE.unsubscribe({room: 'CONNECTED:TotalTime'});
-            SE.unsubscribe({room: 'QUEUEANSWERED:TotalCount'});
-            SE.unsubscribe({room: 'BRIDGE:TotalCount'});
-
-            SE.unsubscribe({room: 'CALLS:TotalCount'});
-            SE.unsubscribe({room: 'CALLS:CurrentCount'});
-            SE.unsubscribe({room: 'QUEUEDROPPED:TotalCount'});
-            SE.unsubscribe({room: 'BRIDGE:CurrentCount'});
-            return;
-        }
-
-        if (view == "queuedetail") {
-            SE.unsubscribe({room: 'QUEUE:QueueDetail'});
-            return;
-        }
+        //if (view == "dashoboard") {
+        //    SE.unsubscribe({room: 'QUEUE:QueueDetail'});
+        //    SE.unsubscribe({room: 'QUEUE:CurrentCount'});
+        //    SE.unsubscribe({room: 'QUEUE:TotalCount'});
+        //    SE.unsubscribe({room: 'QUEUE:TotalCount'});
+        //    SE.unsubscribe({room: 'CONNECTED:TotalTime'});
+        //    SE.unsubscribe({room: 'QUEUEANSWERED:TotalCount'});
+        //    SE.unsubscribe({room: 'BRIDGE:TotalCount'});
+        //
+        //    SE.unsubscribe({room: 'CALLS:TotalCount'});
+        //    SE.unsubscribe({room: 'CALLS:CurrentCount'});
+        //    SE.unsubscribe({room: 'QUEUEDROPPED:TotalCount'});
+        //    SE.unsubscribe({room: 'BRIDGE:CurrentCount'});
+        //    return;
+        //}
+        //
+        //if (view == "queuedetail") {
+        //    SE.unsubscribe({room: 'QUEUE:QueueDetail'});
+        //    return;
+        //}
     };
 
     var subscribe = function (view) {
-        if (view == "dashoboard") {
-            SE.unsubscribe({room: 'QUEUE:QueueDetail'});
-            SE.unsubscribe({room: 'QUEUE:CurrentCount'});
-            SE.unsubscribe({room: 'QUEUE:TotalCount'});
-            SE.unsubscribe({room: 'QUEUE:TotalCount'});
-            SE.unsubscribe({room: 'CONNECTED:TotalTime'});
-            SE.unsubscribe({room: 'QUEUEANSWERED:TotalCount'});
-            SE.unsubscribe({room: 'BRIDGE:TotalCount'});
-
-            SE.unsubscribe({room: 'CALLS:TotalCount'});
-            SE.unsubscribe({room: 'CALLS:CurrentCount'});
-            SE.unsubscribe({room: 'QUEUEDROPPED:TotalCount'});
-            SE.unsubscribe({room: 'BRIDGE:CurrentCount'});
-            return;
-        }
-
-        if (view == "queuedetail") {
-            SE.unsubscribe({room: 'QUEUE:QueueDetail'});
-            return;
-        }
+        //if (view == "dashoboard") {
+        //    SE.unsubscribe({room: 'QUEUE:QueueDetail'});
+        //    SE.unsubscribe({room: 'QUEUE:CurrentCount'});
+        //    SE.unsubscribe({room: 'QUEUE:TotalCount'});
+        //    SE.unsubscribe({room: 'QUEUE:TotalCount'});
+        //    SE.unsubscribe({room: 'CONNECTED:TotalTime'});
+        //    SE.unsubscribe({room: 'QUEUEANSWERED:TotalCount'});
+        //    SE.unsubscribe({room: 'BRIDGE:TotalCount'});
+        //
+        //    SE.unsubscribe({room: 'CALLS:TotalCount'});
+        //    SE.unsubscribe({room: 'CALLS:CurrentCount'});
+        //    SE.unsubscribe({room: 'QUEUEDROPPED:TotalCount'});
+        //    SE.unsubscribe({room: 'BRIDGE:CurrentCount'});
+        //    return;
+        //}
+        //
+        //if (view == "queuedetail") {
+        //    SE.unsubscribe({room: 'QUEUE:QueueDetail'});
+        //    return;
+        //}
     };
 
     return {
