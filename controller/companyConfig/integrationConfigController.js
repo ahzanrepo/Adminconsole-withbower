a18b14410dcaa7e798dab02c4014f5ea1ccec632
waruna <waruna@duosoftware.com>
2017-07-04 12:43:07 +0530

    save integration's  configuration data add tooltips and UI fine tune

--- controller/companyConfig/integrationConfigController.js
+++ controller/companyConfig/integrationConfigController.js
@@ -2,11 +2,36 @@
  * Created by Waruna on 7/3/2017.
  */
 
-mainApp.controller("integrationConfigController", function ($scope, $filter, $location, $log, $anchorScroll, resourceProductivityService) {
+mainApp.controller("integrationConfigController", function ($scope, $filter, $location, $log, $anchorScroll, integrationConfigService) {
 
     $anchorScroll();
 
-    $scope.showAlert = function (tittle, content, type) {
+    $scope.showConfirm = function (tittle, label, okbutton, cancelbutton, content, OkCallback, CancelCallBack, okObj) {
+
+        (new PNotify({
+            title: tittle,
+            text: content,
+            icon: 'glyphicon glyphicon-question-sign',
+            hide: false,
+            confirm: {
+                confirm: true
+            },
+            buttons: {
+                closer: false,
+                sticker: false
+            },
+            history: {
+                history: false
+            }
+        })).get().on('pnotify.confirm', function () {
+            OkCallback("confirm");
+        }).on('pnotify.cancel', function () {
+
+        });
+
+    };
+
+    $scope.showAlert = function (tittle,type, content) {
 
         new PNotify({
             title: tittle,
@@ -17,7 +42,7 @@ mainApp.controller("integrationConfigController", function ($scope, $filter, $lo
     };
 
     $scope.integrationDataList = [];
-    $scope.integrationData = {method: "GET", parameters:[]};
+    $scope.integrationData = {method: "GET", parameters: []};
     $scope.parameters = {parameterLocation: "QUERY"};
 
     $scope.isProcessing = false;
@@ -39,20 +64,68 @@ mainApp.controller("integrationConfigController", function ($scope, $filter, $lo
         var form = $scope["fInner"];
         form.$setUntouched();
         form.$setPristine();
-
-        //$scope.showParameter = false;
+        $scope.showParameter = false;
     };
 
-    $scope.saveConfig = function (integrationData) {
-
-        var form = $scope["fOuter"];
-        form.$setUntouched();
-        form.$setPristine();
-
-        $scope.integrationDataList.push(integrationData);
+    $scope.loadConfig = function () {
         $scope.isProcessing = true;
-        $scope.showConfiguration = false;
-        $scope.integrationData = {method: "GET", parameters:[]};
+        integrationConfigService.getIntegrationAPIDetails().then(function (response) {
+            if (response) {
+                $scope.integrationDataList = response;
+            } else {
+                $scope.showAlert("Integrations", "error", "Fail To Load Integration Configurations.");
+            }
+            $scope.isProcessing = false;
+        }, function (error) {
+            $scope.isProcessing = false;
+            $scope.showAlert("Integrations", "error", "Fail To Load Integration Configurations.");
+        });
+
+    };
+    $scope.loadConfig();
+
+    $scope.saveConfig = function (integrationData) {
+        $scope.isProcessing = true;
+        integrationConfigService.saveIntegrationAPIDetails(integrationData).then(function (response) {
+            if (response) {
+                $scope.showAlert("Integrations", 'success', "Configurations Created Successfully.");
+                $scope.integrationDataList.push(response);
+                $scope.showConfiguration = false;
+                $scope.integrationData = {method: "GET", parameters: []};
+
+                var form = $scope["fOuter"];
+                form.$setUntouched();
+                form.$setPristine();
+            } else {
+
+                $scope.showAlert("Integrations", "error", "Fail To Save Integration Configurations.");
+            }
+            $scope.isProcessing = false;
+        }, function (error) {
+            $scope.isProcessing = false;
+            $scope.showAlert("Integrations", "error", "Fail To Save Integration Configurations.");
+        });
+
+    };
+
+    $scope.deleteIntegrationAPIDetails = function (integrationData) {
+        $scope.showConfirm("Delete Configurations", "Delete", "ok", "cancel", "Do you want to delete " + integrationData.name, function (obj) {
+            $scope.isProcessing = true;
+            integrationConfigService.deleteIntegrationAPIDetails(integrationData._id).then(function (response) {
+                if (response) {
+                    $scope.loadConfig();
+                    $scope.showAlert("Integrations", 'success', "Configurations Deleted Successfully.");
+                } else {
+                    $scope.showAlert("Integrations", "error", "Fail To Delete Integration Configurations.");
+                }
+                $scope.isProcessing = false;
+            }, function (error) {
+                $scope.isProcessing = false;
+                $scope.showAlert("Integrations", "error", "Fail To Delete Integration Configurations.");
+            });
+        }, function () {
+
+        }, integrationData)
     };
 
     $scope.deleteParameter = function (parameters) {
--- factory/companyConfig/integrationConfigService.js
+++ factory/companyConfig/integrationConfigService.js
@@ -2,30 +2,49 @@
  * Created by Waruna on 7/3/2017.
  * */
 
-mainApp.factory('integrationConfigService', function ($http, authService,baseUrls) {
+mainApp.factory("integrationConfigService", function ($http, authService,baseUrls) {
     return {
 
-        getCloudEndUser: function () {
+        saveIntegrationAPIDetails: function (postData) {
             return $http({
-                method: 'GET',
-                url:baseUrls.clusterconfigUrl +"CloudEndUsers"
+                method: 'POST',
+                url:baseUrls.integrationapi +"IntegrationInfo",
+                data:postData
             }).then(function(response)
             {
-                return response;
+                if (response.data && response.data.IsSuccess) {
+                    return response.data.Result;
+                } else {
+                    return null;
+                }
             });
         },
 
-        saveNewEndUser: function (resource) {
-            
-
+        getIntegrationAPIDetails: function () {
             return $http({
-                method: 'POST',
-                url: baseUrls.clusterconfigUrl +"CloudEndUser",
-                data:resource
-
+                method: 'GET',
+                url: baseUrls.integrationapi +"IntegrationInfo"
             }).then(function(response)
             {
-                return response;
+                if (response.data && response.data.IsSuccess) {
+                    return response.data.Result;
+                } else {
+                    return null;
+                }
+            });
+        },
+
+        deleteIntegrationAPIDetails: function (id) {
+            return $http({
+                method: 'DELETE',
+                url: baseUrls.integrationapi +"IntegrationInfo/"+id
+            }).then(function(response)
+            {
+                if (response.data && response.data.IsSuccess) {
+                    return response.data.IsSuccess;
+                } else {
+                    return false;
+                }
             });
         }
     }
