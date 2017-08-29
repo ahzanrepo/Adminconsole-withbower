a3fc2f197457c1ec743a44f5f0d962eb02081e8a
frodo <dasun@duosoftware.com>
2017-08-29 18:10:00 +0530

    Update navigationTree.js
    bug fix

--- application_access_management/scripts/directives/navigationTree.js
+++ application_access_management/scripts/directives/navigationTree.js
@@ -24,46 +24,67 @@ mainApp.directive("navigationtree", function ($filter, appAccessManageService) {
             scope.vm.data = newItem(0, scope.navigation.navigationName);
 
             var items = $filter('filter')(scope.selectedConsole.consoleNavigation.saveItem, {menuItem: scope.navigation.navigationName})
-
+            if (scope.navigation.navigationName === 'FILE_CAT_RESTRICT') {
+                console.log('sdfdfdfs');
+            }
             /*Generate tree*/
             var id = 1;
+            var rootSelected = true;
             angular.forEach(scope.navigation.resources, function (resource) {
                 var item1 = addChild(scope.vm.data, resource.scopeName, resource.feature);
-
-                var read ,write,del;
-                if (resource.actions.indexOf("read") > -1) {
+                var optionSelected = true;
+                angular.forEach(resource.actions, function (action) {
                     id++;
-                    read = addChild(item1, id, "Read");
-                }
-                if (resource.actions.indexOf("write") > -1) {
-                    id++;
-                    write = addChild(item1, id, "Write");
-                }
-                if (resource.actions.indexOf("delete") > -1) {
-                    id++;
-                    del = addChild(item1, id, "Delete");
-                }
-                if (items)
-                    if (items.length != 0) {
-                        var optionSelected = false;
-                        angular.forEach(items[0].menuAction, function (action) {
-                            if(read){
-                                read.isSelected = action.read;
-                            }
-                            if(write){
-                                write.isSelected = action.write;
-                            }
-                            if(del){
-                                del.isSelected = action.delete;
-                            }
-                            optionSelected = action.read ? true : (action.write ? true : action.delete)
-                        });
-                        if (optionSelected) {
-                            item1.isSelected = true;
-                            scope.vm.data.isSelected = true;
-                        }
+                    var child = addChild(item1, id, action);
+                    if (items.length > 0 && items[0].menuAction) {
+                        var menuItems = $filter('filter')(items[0].menuAction, {scope: resource.scopeName},true);
+                        child.isSelected = menuItems[0][action];
                     }
+                    optionSelected = optionSelected && child.isSelected;
+                });
+
+                item1.isSelected = optionSelected;
+                rootSelected = rootSelected && item1.isSelected;
+                scope.vm.data.isSelected = rootSelected;
+                /*var read, write, del;
+                 if (resource.actions.indexOf("read") > -1) {
+                 id++;
+                 read = addChild(item1, id, "Read");
+                 }
+                 if (resource.actions.indexOf("write") > -1) {
+                 id++;
+                 write = addChild(item1, id, "Write");
+                 }
+                 if (resource.actions.indexOf("delete") > -1) {
+                 id++;
+                 del = addChild(item1, id, "Delete");
+                 }
+                 if (items)
+                 if (items.length != 0) {
+                 var optionSelected = false;
+                 angular.forEach(items[0].menuAction, function (action) {
+                 if (read) {
+                 read.isSelected = action.read;
+                 optionSelected = action.read;
+                 }
+                 if (write) {
+                 write.isSelected = action.write;
+                 optionSelected = action.write && (read && read.isSelected);
+                 }
+                 if (del) {
+                 del.isSelected = action.delete;
+                 optionSelected = action.delete && (write && write.isSelected) && (read && read.isSelected);
+                 }
+                 //optionSelected = action.read ? true : (action.write ? true : action.delete)
+                 });
+                 if (optionSelected) {
+                 item1.isSelected = true;
+                 scope.vm.data.isSelected = true;
+                 }
+                 }*/
             });
+
+
 
             /*scope.vm.expandAll(scope.vm.data);*/
 
@@ -101,46 +122,71 @@ mainApp.directive("navigationtree", function ($filter, appAccessManageService) {
                         "menuItem": navigationData.name,//"navigationName": "ARDS_CONFIGURATION",
                         "menuAction": []
                     };
-                    if(navigationData.isSelected) {
+                    if (navigationData.isSelected) {
                         angular.forEach(navigationData.children, function (menu) {
-                            var data = {};
-                            data = {
+                            /*var data = {
+                             "scope": menu.id,//"scopeName": "requestmeta",
+                             "read": (menu.children["0"])? menu.children["0"].isSelected: false,
+                             "write": (menu.children["1"])?menu.children["1"].isSelected: false,
+                             "delete": (menu.children["2"])?menu.children["2"].isSelected: false
+                             };*/
+
+                            var data = {
                                 "scope": menu.id,//"scopeName": "requestmeta",
-                                "read": menu.children["0"].isSelected,
-                                "write": menu.children["1"].isSelected,
-                                "delete": menu.children["2"].isSelected,
+                                "read": false,
+                                "write": false,
+                                "delete": false
                             };
+                            angular.forEach(menu.children, function (item) {
+                                data[item.name.toLowerCase()] = item.isSelected;
+                            });
+
                             editedMenus.menuAction.push(data);
                         });
 
                         appAccessManageService.AddSelectedNavigationToUser(scope.userName, scope.consoleName, editedMenus).then(function (response) {
-                            if (response) {
+                            if (response.IsSuccess) {
                                 scope.showAlert("Info", "Info", "ok", navigationData.name + " Successfully Updated.")
                             }
                             else {
-                                scope.showError("Error", "Error", "ok", navigationData.name + " Fail To Update.");
+                                if(response.CustomMessage)
+                                {
+                                    scope.showError("Error",  response.CustomMessage);
+                                }
+                                else {
+                                    scope.showError("Error",  navigationData.name + " Failed To Update.");
+                                }
+
                             }
 
                         }, function (error) {
-                            scope.showError("Error", "Error", "ok", "There is an error. Fail to Add Permissions[" + navigationData.name + "]");
+                            scope.showError("Error", "Failed to Add Permissions[" + navigationData.name + "]");
                         });
                     }
-                    else{
+                    else {
                         appAccessManageService.DeleteSelectedNavigationFrmUser(scope.userName, scope.consoleName, navigationData.name).then(function (response) {
-                            if (response) {
+                            if (response.IsSuccess) {
                                 scope.showAlert("Info", "Info", "ok", navigationData.name + " Permissions Successfully Remove.")
                             }
                             else {
-                                scope.showError("Error", "Error", "ok", navigationData.name + " Fail To Update.");
+                                if(response.CustomMessage)
+                                {
+                                    scope.showError("Error", response.CustomMessage);
+                                }
+                                else
+                                {
+                                    scope.showError("Error", navigationData.name + " Fail To Update.");
+                                }
+
                             }
 
                         }, function (error) {
-                            scope.showError("Error", "Error", "ok", "There is an error. Fail to Remove Permissions[" + navigationData.name + "]");
+                            scope.showError("Error"," Failed to Remove Permissions[" + navigationData.name + "]");
                         });
                     }
                 }
                 catch (ex) {
-                    scope.showError("Error", "Error", "ok", "There is an error. Fail to Add Permissions["+navigationData.name +"]");
+                    scope.showError("Error", "Failed to Add Permissions[" + navigationData.name + "]");
                     console.error(ex);
                 }
             };
