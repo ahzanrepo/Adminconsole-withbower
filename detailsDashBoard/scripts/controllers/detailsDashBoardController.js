1cb8dc7660cf96f05f44f602a40e5ef06abadb18
waruna <waruna@duosoftware.com>
2018-01-09 15:25:06 +0530

    add agent profile information to details dashboard

--- detailsDashBoard/scripts/controllers/detailsDashBoardController.js
+++ detailsDashBoard/scripts/controllers/detailsDashBoardController.js
@@ -2,7 +2,7 @@
  * Created by Waruna on 9/27/2017.
  */
 
-mainApp.controller("detailsDashBoardController", function ($scope, $rootScope, $filter, $stateParams, $anchorScroll, $timeout, $q, uiGridConstants, queueMonitorService, subscribeServices, agentStatusService, contactService, cdrApiHandler,ShareData) {
+mainApp.controller("detailsDashBoardController", function ($scope, $rootScope, $filter, $stateParams, $anchorScroll, $timeout, $q, uiGridConstants, queueMonitorService, subscribeServices, agentStatusService, contactService, cdrApiHandler, ShareData) {
     $anchorScroll();
 
     $scope.dtOptions = {paging: false, searching: false, info: false, order: [0, 'desc']};
@@ -247,7 +247,7 @@ mainApp.controller("detailsDashBoardController", function ($scope, $rootScope, $
             angular.forEach($scope.onlineProfile, function (agent) {
                 try {
 
-                    if (agent && (agent.BusinessUnit.toLowerCase() === ShareData.BusinessUnit.toLowerCase()||ShareData.BusinessUnit.toLowerCase()==="all")) {
+                    if (agent && (agent.BusinessUnit.toLowerCase() === ShareData.BusinessUnit.toLowerCase() || ShareData.BusinessUnit.toLowerCase() === "all")) {
 
 
                         var ids = $filter('filter')($scope.productivity, {ResourceId: agent.ResourceId.toString()}, true);//"ResourceId":"1"
@@ -464,6 +464,7 @@ mainApp.controller("detailsDashBoardController", function ($scope, $rootScope, $
     $scope.onlineProfile = [];
 
     $scope.getProfileDetails = function () {
+        $scope.onlineProfile = [];
         var deferred = $q.defer();
         agentStatusService.GetProfileDetails().then(function (response) {
             if (response) {
@@ -505,7 +506,7 @@ mainApp.controller("detailsDashBoardController", function ($scope, $rootScope, $
 
     // getAllRealTime();
     var getAllRealTimeTimer = $timeout(getAllRealTime, $scope.refreshTime);
-
+    $scope.BusinessUnitUsers = [];
     $scope.gridOptions = {
         enableColumnResizing: true,
         enableRowSelection: true,
@@ -668,6 +669,16 @@ mainApp.controller("detailsDashBoardController", function ($scope, $rootScope, $
                     $scope.GetCallLogs(1, row.entity.ResourceName);
                     $scope.gridTaskOptions.data = $scope.selectedAgent.taskList;
                     $scope.getAgentStatusList($scope.selectedAgent);
+
+
+                    var ids = $filter('filter')($scope.BusinessUnitUsers, {resourceid: row.entity.ResourceId.toString()}, true);//"ResourceId":"1"
+                    if (ids.length > 0) {
+                        var temData = ids[0];
+                        $scope.selectedAgent.displayName = temData.firstname + " " + temData.lastname;
+                        $scope.selectedAgent.avatar = temData.avatar;
+                        if (temData.email)
+                            $scope.selectedAgent.Email = temData.email.contact;
+                    }
                 }
             });
         }
@@ -772,6 +783,317 @@ mainApp.controller("detailsDashBoardController", function ($scope, $rootScope, $
         });
     };
 
+    /*Productivity Chart Config*/
+    var theme = {
+        color: [
+            '#db4114', '#f8b01d', '#2ba89c', '#114858',
+            '#9B59B6', '#8abb6f', '#759c6a', '#bfd3b7'
+        ],
+        title: {
+            itemGap: 8,
+            textStyle: {
+                color: '#408829',
+                fontFamily: 'Roboto',
+                fontWeight: 300
+            }
+        },
+
+        dataRange: {
+            color: ['#1f610a', '#97b58d']
+        },
+
+        toolbox: {
+            color: ['#408829', '#408829', '#408829', '#408829']
+        },
+
+        tooltip: {
+            backgroundColor: 'rgba(0,0,0,0.5)',
+            axisPointer: {
+                type: 'line',
+                lineStyle: {
+                    color: '#408829',
+                    type: 'dashed'
+                },
+                crossStyle: {
+                    color: '#408829'
+                },
+                shadowStyle: {
+                    color: 'rgba(200,200,200,0.3)'
+                }
+            }
+        },
+
+        dataZoom: {
+            dataBackgroundColor: '#eee',
+            fillerColor: 'rgba(64,136,41,0.2)',
+            handleColor: '#408829'
+        },
+        grid: {
+            borderWidth: 0
+        },
+
+        categoryAxis: {
+            axisLine: {
+                lineStyle: {
+                    color: '#408829'
+                }
+            },
+            splitLine: {
+                lineStyle: {
+                    color: ['#eee']
+                }
+            }
+        },
+
+        valueAxis: {
+            axisLine: {
+                lineStyle: {
+                    color: '#408829'
+                }
+            },
+            splitArea: {
+                show: true,
+                areaStyle: {
+                    color: ['rgba(250,250,250,0.1)', 'rgba(200,200,200,0.1)']
+                }
+            },
+            splitLine: {
+                lineStyle: {
+                    color: ['#eee']
+                }
+            }
+        },
+        timeline: {
+            lineStyle: {
+                color: '#408829'
+            },
+            controlStyle: {
+                normal: {color: '#408829'},
+                emphasis: {color: '#408829'}
+            }
+        },
+
+        k: {
+            itemStyle: {
+                normal: {
+                    color: '#68a54a',
+                    color0: '#a9cba2',
+                    lineStyle: {
+                        width: 1,
+                        color: '#408829',
+                        color0: '#86b379'
+                    }
+                }
+            }
+        },
+        map: {
+            itemStyle: {
+                normal: {
+                    areaStyle: {
+                        color: '#ddd'
+                    },
+                    label: {
+                        textStyle: {
+                            color: '#c12e34'
+                        }
+                    }
+                },
+                emphasis: {
+                    areaStyle: {
+                        color: '#99d2dd'
+                    },
+                    label: {
+                        textStyle: {
+                            color: '#c12e34'
+                        }
+                    }
+                }
+            }
+        },
+        force: {
+            itemStyle: {
+                normal: {
+                    linkStyle: {
+                        strokeColor: '#408829'
+                    }
+                }
+            }
+        },
+        chord: {
+            padding: 4,
+            itemStyle: {
+                normal: {
+                    lineStyle: {
+                        width: 1,
+                        color: 'rgba(128, 128, 128, 0.5)'
+                    },
+                    chordStyle: {
+                        lineStyle: {
+                            width: 1,
+                            color: 'rgba(128, 128, 128, 0.5)'
+                        }
+                    }
+                },
+                emphasis: {
+                    lineStyle: {
+                        width: 1,
+                        color: 'rgba(128, 128, 128, 0.5)'
+                    },
+                    chordStyle: {
+                        lineStyle: {
+                            width: 1,
+                            color: 'rgba(128, 128, 128, 0.5)'
+                        }
+                    }
+                }
+            }
+        },
+        gauge: {
+            startAngle: 225,
+            endAngle: -45,
+            axisLine: {
+                show: true,
+                lineStyle: {
+                    color: [[0.2, '#86b379'], [0.8, '#68a54a'], [1, '#408829']],
+                    width: 8
+                }
+            },
+            axisTick: {
+                splitNumber: 10,
+                length: 12,
+                lineStyle: {
+                    color: 'auto'
+                }
+            },
+            axisLabel: {
+                textStyle: {
+                    color: 'auto'
+                }
+            },
+            splitLine: {
+                length: 18,
+                lineStyle: {
+                    color: 'auto'
+                }
+            },
+            pointer: {
+                length: '90%',
+                color: 'auto'
+            },
+            title: {
+                textStyle: {
+                    color: '#333'
+                }
+            },
+            detail: {
+                textStyle: {
+                    color: 'auto'
+                }
+            }
+        },
+        textStyle: {
+            fontFamily: 'Arial, Verdana, sans-serif'
+        }
+    };
+
+    function secondsToTime(secs) {
+        var hours = Math.floor(secs / (60 * 60));
+
+        var divisor_for_minutes = secs % (60 * 60);
+        var minutes = Math.floor(divisor_for_minutes / 60);
+
+        var divisor_for_seconds = divisor_for_minutes % 60;
+        var seconds = Math.ceil(divisor_for_seconds);
+
+        return hours + ":" + minutes + ":" + seconds;
+    }
+
+    $scope.echartDonutSetOption = function (productivity) {
+        var myObject = {};
+        myObject[productivity.Chatid] = echarts.init(document.getElementById("productivityId"), theme);
+        myObject[productivity.Chatid].setOption({
+            title: {
+                show: true,
+                text: productivity.ResourceName,
+                textStyle: {
+                    fontSize: 18,
+                    fontWeight: 'bolder',
+                    color: '#333',
+                    fontFamily: 'Ubuntu-Regular'
+                }
+            },
+            tooltip: {
+                trigger: 'item',
+                //formatter: "{a} <br/>{b} : {c} ({d}%)",
+                formatter: function (params, ticket, callback) {
+                    var res = params.seriesName + ' <br/>' + params.name + ' ' + secondsToTime(params.value) + ' ' + params.percent + '%';
+                    setTimeout(function () {
+                        callback(ticket, res);
+                    }, 100);
+                    return 'loading';
+                }
+            },
+            calculable: true,
+            legend: {
+                x: 'center',
+                y: 'bottom',
+                data: ['After work', 'Break', 'Inbound', 'Outbound', 'Idle', 'Hold']
+            },
+            toolbox: {
+                show: true,
+                feature: {
+                    mark: {show: true},
+                    //dataView : {show: true, readOnly: false},
+                    magicType: {
+                        show: true,
+                        type: ['pie', 'funnel'],
+                        option: {
+                            funnel: {
+                                x: '10%',
+                                width: '50%',
+                                funnelAlign: 'center',
+                                max: 1548
+                            }
+                        }
+                    },
+                    restore: {
+                        show: false,
+                        title: "Restore"
+                    },
+                    saveAsImage: {
+                        show: true,
+                        title: "Save As Image"
+                    }
+                }
+            },
+            series: [{
+                name: 'Productivity',
+                type: 'pie',
+                radius: ['35%', '55%'],
+                itemStyle: {
+                    normal: {
+                        label: {
+                            show: true
+                        },
+                        labelLine: {
+                            show: true
+                        }
+                    },
+                    emphasis: {
+                        label: {
+                            show: true,
+                            position: 'center',
+                            textStyle: {
+                                fontSize: '14',
+                                fontWeight: 'normal'
+                            }
+                        }
+                    }
+                },
+                data: productivity.data
+            }]
+        });
+    };
 
     /*------------------------ Agent info ------------------------------------*/
 
@@ -817,6 +1139,7 @@ mainApp.controller("detailsDashBoardController", function ($scope, $rootScope, $
                 for (var key in agentListResp.Result) {
                     $scope.gridAgentLogsOptions.data = agentListResp.Result[key];
                 }
+                $scope.echartDonutSetOption(selectedAgent)
             }).catch(function (err) {
                 $scope.showAlert('Error', 'error', 'Error occurred while loading agent status events');
             });
@@ -831,22 +1154,26 @@ mainApp.controller("detailsDashBoardController", function ($scope, $rootScope, $
 
     /*------------------------ getAgentStatusList ------------------------------------*/
 
-    $scope.BusinessUnitUsers = [];
+    var GetUserByBusinessUnit = function () {
+        ShareData.GetUserByBusinessUnit().then(function (respond) {
+            $scope.BusinessUnitUsers = respond;
+        }, function (error) {
+
+        });
+    };
+    GetUserByBusinessUnit();
+
     /*Listing For Business Unit Change*/
     $scope.$watch(function () {
         return ShareData.BusinessUnit;
     }, function (newValue, oldValue) {
         if (newValue.toString().toLowerCase() != oldValue.toString().toLowerCase()) {
+            $scope.selectedAgent = null;
             $scope.GetAllQueueStatistics();
             getAllRealTime();
-            /*ShareData.GetUserByBusinessUnit().then(function (respons) {
-                $scope.BusinessUnitUsers = respons;
-            }, function (error) {
-
-            });*/
+            GetUserByBusinessUnit();
             console.log("Reload Dashboard ****************************************");
         }
-
     });
 });
 
